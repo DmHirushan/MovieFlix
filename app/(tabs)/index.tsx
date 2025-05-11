944165f36e396d3c704322974d29fdca46764d40
DmHirushan <dimuthsamaranayaka756@gmail.com>
2025-05-11 21:05:17 +0530

    movies fetchde and displayed

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import {ActivityIndicator, Animated, FlatList, Image, Text, View} from "react-native";
+ import {ActivityIndicator, Animated, FlatList, Image, Text, View} from "react-native";
 import React from "react";
 import {Link} from "expo-router";
 import {images} from "@/constants/images";
